93baee5ee1f1d1bfabb1dc829d8534410be68cde
atulRanaa <atul.rana@go-mmt.com>
2019-09-28 15:10:58 +0530

    [NEW] New problem solved

---
+++ HackerEarth/JoeyEquilateralTriangle.cpp
@@ -0,0 +1,53 @@
+// hackerearth
+        #include <bits/stdc++.h>
+             
+        using namespace std;
+         
+        typedef long long ll; 
+        const ll N = 1e6+5;
+        const ll MOD = 1000000007;
+        #define all(x) x.begin(),x.end()
+        #define pii pair<int,int>
+        #define pis pair<ll,string>
+        #define F first
+        #define S second
+        #define LCM(a,b) ((a*b)/__gcd(a,b))
+        #define inf 1e15
+        #define test ll cse;cin>>cse;for(ll _i=1;_i<=cse;_i++)
+        #define PI 3.14159265
+        #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+        const double EPS = 1E-9;
+        typedef vector<vector<double>> matrix;
+        typedef vector<int> vi;
+
+
+        // ll ar[N];
+        // ll ans[N];     
+        // struct node{
+        //     ll val;
+        //     node *L,*R;
+        //     node(ll x){val = x; L=R=NULL;}
+        // };
+        // int j=2;
+        // node* insert(node* root,ll data,ll lvl){
+        //     if (root == NULL) { ans[j++] = lvl;return (new node(data));}
+        //     else{
+        //         if (data <= root->val) root->L = insert(root->L, data, lvl+1);
+        //         else                   root->R = insert(root->R, data, lvl+1);
+        //         return root;
+        //     }
+        // }
+        // node* make(ll n){
+        //     node *root = new node(ar[1]);
+        //     for(int i=2;i<=n;i++)
+        //         root = insert(root, ar[i],1);
+        //     return root;
+        // }
+
+int main(){
+    fast;
+    ll n;
+    test{
+        cin >> n; cout << "NO\n";
+    }
+}
